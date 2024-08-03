51acac3f8d959c3721dbd422178f9bd9087afdbb
Diviyan20 <diviyanr@gmail.com>
2024-08-03 21:40:16 +0800

    feat: Create Game Over Screen

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -5,18 +5,11 @@ public class MainMenuManager : MonoBehaviour
 {
     public void PlayGame()
     {
-        Debug.Log("Play Button Clicked!");
         SceneManager.LoadScene("Game");
     }
 
     public void OptionsButton()
     {
         Debug.Log("Options Button Clicked!");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
