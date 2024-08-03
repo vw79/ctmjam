e2c7beb0964c88bb663610184cbc2e30f8dd5f96
Diviyan20 <diviyanr@gmail.com>
2024-08-03 10:57:02 +0800

    feat: Parallax Background

--- Assets/Scripts/Background/BackgroundScroller.cs
+++ Assets/Scripts/Background/BackgroundScroller.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(-1f,1f)]
+    [SerializeField] private float scrollSpeed = 0.5f;
+    private float offset;
+    private Material mat;
     void Start()
     {
-        
+        mat = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset,0));
         
     }
 }
