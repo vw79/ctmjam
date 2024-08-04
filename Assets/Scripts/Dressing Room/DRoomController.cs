6951cc335ac3a96bcdaedbd863a260320206c25d
DanzKhan22 <116078811+DanzKhan22@users.noreply.github.com>
2024-08-04 17:34:06 +0800

    k

--- Assets/Scripts/Dressing Room/DRoomController.cs
+++ Assets/Scripts/Dressing Room/DRoomController.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 [System.Serializable]
 public class SkinDeets{
-    public GameObject Skin;
+    public Sprite prefabSprite;
     public string Name;
     public bool IsOwned;
 
@@ -19,37 +19,78 @@ public class DRoomController : MonoBehaviour
     public Button prevBtn;
     public Button SelectBtn;
     public TextMeshProUGUI SkinName;
-    private GameObject DisplayModel;
+    public GameObject DisplayModelBase;
+    public GameObject DisplayModel1;
+    public GameObject DisplayModel2;
+    public GameObject DisplayModel3;
     private int CurrentIndex=0;
+
 
     private void Start() {
         ShowCurrentSkin();
+        DisplayModel1.SetActive(false);
+        DisplayModel2.SetActive(false);
+        DisplayModel3.SetActive(false);
+        DisplayModelBase.SetActive(true);
+
     }
     public void ShowCurrentSkin(){
 
-        if (CurrentIndex >= 0 && CurrentIndex < skinDeets.Length)
+        if (CurrentIndex == 0)
         {
-            SkinDeets CurrentSkin = skinDeets[CurrentIndex];
-            if (DisplayModel != null) 
-            {
-                DisplayModel.SetActive(false);
-                DisplayModel = CurrentSkin.Skin;
-                DisplayModel.SetActive(true);
-            }
-            else { 
-                DisplayModel = CurrentSkin.Skin;
-                DisplayModel.SetActive(true);
-            }
-            SkinName.text = (CurrentSkin.Name);
-            if(CurrentSkin.IsOwned){
-                SelectBtn.interactable = false;
-            }else{
-                SelectBtn.interactable = true;
-            }
-            nextBtn.interactable = CurrentIndex > 0;
-            prevBtn.interactable = CurrentIndex < skinDeets.Length - 1;
-
+            DisplayModelBase .SetActive(true);
+            DisplayModel1.SetActive(false);
+            DisplayModel2.SetActive(false);
+            DisplayModel3.SetActive(false);
         }
+
+        if (CurrentIndex == 1)
+        {
+            DisplayModelBase.SetActive(false);
+            DisplayModel1.SetActive(true);
+            DisplayModel2.SetActive(false);
+            DisplayModel3.SetActive(false);
+        }
+
+        if (CurrentIndex == 2)
+        {
+            DisplayModelBase.SetActive(false);
+            DisplayModel1.SetActive(false);
+            DisplayModel2.SetActive(true);
+            DisplayModel3.SetActive(false);
+        }
+
+        if (CurrentIndex == 3)
+        {
+            DisplayModelBase.SetActive(false);
+            DisplayModel1.SetActive(false);
+            DisplayModel2.SetActive(false);
+            DisplayModel3.SetActive(true);
+        }
+
+        //if (CurrentIndex >= 0 && CurrentIndex < skinDeets.Length)
+        //{
+        //    SkinDeets CurrentSkin = skinDeets[CurrentIndex];
+        //    if (DisplayModel != null) 
+        //    {
+        //        DisplayModel.SetActive(false);
+        //        imgRenderer.sprite = CurrentSkin.prefabSprite;
+        //        DisplayModel.SetActive(true);
+        //    }
+        //    else {
+        //        imgRenderer.sprite = CurrentSkin.prefabSprite;
+        //        DisplayModel.SetActive(true);
+        //    }
+        //    SkinName.text = (CurrentSkin.Name);
+        //    if(CurrentSkin.IsOwned){
+        //        SelectBtn.interactable = false;
+        //    }else{
+        //        SelectBtn.interactable = true;
+        //    }
+        //    nextBtn.interactable = CurrentIndex > 0;
+        //    prevBtn.interactable = CurrentIndex < skinDeets.Length - 1;
+
+        //}
     }
 
     public void OnNextBtnPressed(){
